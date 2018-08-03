4738af591bbd207ecdee92be18b7597360698a0a
Pooja-Naidu <30519958+Pooja-Naidu@users.noreply.github.com>
2018-08-03 19:49:04 +0530

    Add files via upload
    second commit

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 $(document).ready(function(){
-	$.ajax({
-		url: "http://localhost/mychart/js/data.php",
-		method: "GET",
-		success: function(data) {
-			console.log(data);
-			var updatedate = [];
-			var total = [];
+  $.ajax({ 
+  url:"http://localhost/chart/data.php",
+    method:"GET",
+    success: function(data) {
+      console.log(data);
+      var udate = [] ;
+      var ttl = [] ;
 
-			for(var i in data) {
-				date.push("updatedate " + data[i].updatedate);
-				total.push(data[i].total);
-			}
+      for(var i in data){
+        udate.push(data[i].updatedate);
+        ttl.push(data[i].total);
+      }
+      var chartdata = {
+        labels: udate,
+        datasets : [
+         { 
+          label: 'update',
+          backgroundColor: 'rgba(200,200,200,0.75)',
+          borderColor: 'rgba(200,200,200,0.75)',
+          hoverBackgroundColor: 'rgba(200,200,200,1)',
+          hoverBorderColor: 'rgba(200,200,200,1)',
+          data:ttl
 
-			var chartdata = {
-				labels: updatedate,
-				datasets : [
-					{
-						label: 'updatedate',
-						backgroundColor: 'rgba(200, 200, 200, 0.75)',
-						borderColor: 'rgba(200, 200, 200, 0.75)',
-						hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
-						hoverBorderColor: 'rgba(200, 200, 200, 1)',
-						data: total
-					}
-				]
-			};
+        }
 
-			var ctx = $("#mycanvas");
-
-			var barGraph = new Chart(ctx, {
-				type: 'bar',
-				data: chartdata
-			});
-		},
-		error: function(data) {
-			console.log(data);
-		}
-	});
-});
+      ]
+    };
+    var ctx = $("#mycanvas");
+    var barGraph = new Chart(ctx,{
+      type: 'bar',
+      data: chartdata
+    });
+  },
+      error: function(data){
+        console.log(data);
+      }
+  
+  }); 
+      });
+    	
--- data.php
+++ data.php
@@ -1,23 +1,10 @@
-
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 $conn=new mysqli('localhost','root','','electiontracker');
-if($conn->connect_error){
-    die("Connection failed:".$conn->connect_error);
-}
-$sql="SELECT updatedate,total from updatetable where SNo='ST-001'";
+$sql="SELECT updatedate,total from updatetable where SNo='ST-005'";
 $res=$conn->query($sql);
 $outp = array();
-if ( $res->num_rows > 0){
- $outp = $res->fetch_all(MYSQLI_ASSOC);
+$outp = $res->fetch_all(MYSQLI_ASSOC);
 echo json_encode($outp);
-//while ($row = $res->fetch_assoc()) {
-    //echo "SNo:".$row["updatedate"]."Status:".$row["total"]."<br>";
-//}
-}
-else {
-    echo "0 results";
-}
-$conn->close();
 
 ?>
